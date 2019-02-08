fe5a73094369afd66387cc6b5ea4c5c705dfc83b
Shane Canon <scanon@lbl.gov>
2019-02-08 07:47:30 -0800

    LDAP Tweaks
    - Switch to nim-ldap tree and make the ldap base configurable by an
      environment variable.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ ! -z "$LDAP_BASE" ] ; then
+  echo "Replacing LDAP BASE with $LDAP_BASE"
+  sed -i "s/ou=nim-ldap,ou=host,o=ldapsvc,dc=nersc,dc=gov/${LDAP_BASE}/" /etc/nslcd.conf
+  sed -i "s/ou=nim-ldap,ou=host,o=ldapsvc,dc=nersc,dc=gov/${LDAP_BASE}/" /etc/pam_ldap.conf
+
+fi
+
 nslcd &
 
 /usr/local/bin/gunicorn -b 0.0.0.0:5000 --log-level INFO api:app
