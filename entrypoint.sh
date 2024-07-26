0262873624033ae34c13791acceff565af2570fb
Shane Canon <scanon@lbl.gov>
2024-07-26 08:27:53 -0700

    Update Entrypoint
    Some LDAP configurations weren't being updated.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 
 
 
+if [ ! -z "$OTP_PROXY_URL" ] ; then
+  echo "Replacing OTP Proxy with $OTP_PROXY_URL"
+  sed -i "s|https://otpproxy.nersc.gov|$OTP_PROXY_URL|" /etc/pam.d/sshauth
+fi
+
 if [ ! -z "$LDAP_BASE" ] ; then
   echo "Replacing LDAP BASE with $LDAP_BASE"
   sed -i "s/ou=nim-ldap,ou=host,o=ldapsvc,dc=nersc,dc=gov/${LDAP_BASE}/" /etc/nslcd.conf
   sed -i "s/ou=nim-ldap,ou=host,o=ldapsvc,dc=nersc,dc=gov/${LDAP_BASE}/" /etc/pam_ldap.conf
-
-fi
-
-if [ ! -z "$OTP_PROXY_URL" ] ; then
-  echo "Replacing OTP Proxy with $OTP_PROXY_URL"
-  sed -i "s|https://otpproxy.nersc.gov|$OTP_PROXY_URL|" /etc/pam.d/sshauth
+  sed -i "s/ou=cori,ou=Host,o=ldapsvc,dc=nersc,dc=gov/${LDAP_BASE}/" /etc/pam.d/sshauth
 fi
 
 nslcd &
